7e60f218d731539479e50eb339a630217360dbd7
kanakagrawal <kanakagrawal15140@gmail.com>
2017-10-14 18:04:35 +0530

    Create Account Completed

--- register.php
+++ register.php
@@ -1,44 +1,43 @@
-<!DOCTYPE html>
-<html >
-  <head>
-    <meta charset="UTF-8">
-    <title>Register | Instructor</title>
-     <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <style>
-          .nav-tabs li {
-              width: 50%;
-            }
-        </style>
-  </head>
-  <body>
-    <div class="container">
-        <div class="col-md-6 col-md-offset-3">
-            <h1>Instructor Login</h1>
-            <ul class="nav nav-tabs">
-                <li><a href="login.php">Login</a></li>
-                <li class="active"><a href="register.php">Register</a></li>
-            </ul>
-            <br>
-            <form action="{% url 'feeder:instructor_register' %}" method="post">
-                  {% csrf_token %}
-                 {% if error_message %}<p><strong>{{ error_message }}</strong></p>{% endif %}
-                  {% for field in form %}
-                    <div class="form-group">
-                        {% if field.errors %} <p style="color: rgb(255,0,0)">{{field.errors}}</p>{% endif %}
-                        {{ field.label_tag }} {{ field }}
-                        {% if field.help_text %}
-                        <p class="help">{{ field.help_text|safe }}</p>
-                        {% endif %}
-                    </div>
-                  {% endfor %}
-                <div align="center">
-                    <button type="submit" class="btn btn-success">Register</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</body>
-</html>
+<?php 
+  if(!empty($_POST)){
+    session_start();
+    require("db_connect.php");
+    $query = "SELECT * FROM person WHERE email = '".$_POST["email"]."'";
+    $result = pg_query($con, $query);
+    $row    = pg_fetch_assoc($result);
+    if(empty($row)){
+      $email_not_available = false;
+      $pass = $_POST["password"];
+      $repass = $_POST["password_again"];
+      if(strcmp($pass,$repass)==0){
+        $pass_match = true;
+        // $pass = hash('sha256',$pass);
+        $query = "INSERT INTO person VALUES('".$_POST["email"]."', '".$_POST["name"]."', ".
+                  $_POST["phone"].", '".$_POST["credit_number"]."', '".$pass."')";
+        $result = pg_query($con, $query);
+        if(!$result){
+          echo "Error";
+          exit;
+        }
+        else{
+          $_SESSION['email'] = $_POST["email"];
+          require("db_close.php");        
+          header("Location: http://localhost:8080/app/login.php");
+          exit;
+        }
+      }
+      else{
+        $pass_match = false;
+      }
+    }
+    else{
+      $email_not_available = true;
+    }
+    require("db_close.php");
+  }
+  else{
+    $email_not_available = false;
+    $pass_match = true;
+  }
+  require("registerpage.php");
+?>
---
+++ registerpage.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Appname Registration</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/css/materialize.min.css">
+
+<style type="text/css">
+html,
+body {
+    height: 100%;
+}
+html {
+    display: table;
+    margin: auto;
+}
+body {
+    display: table-cell;
+    vertical-align: middle;
+}
+.margin {
+  margin: 0 !important;
+}
+</style>
+  
+</head>
+
+<body class="white">
+    <div class="col s12 z-depth-6 card-panel">
+      <form class="login-form" method="post">
+        <div class="row">
+          <div class="input-field col s12 center">
+            <img src="logo.png" alt="" class="responsive-img valign profile-image-login" style="padding: 5%; width:400px;height:150px;">
+            <p class="center login-form-text"></p>
+          </div>
+        </div>
+        
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="mdi-social-person-outline prefix"></i>
+            <input class="validate" id="email" type="email" name="email">
+            <label for="email">Email</label>
+          </div>
+        </div>
+
+        <?php if($email_not_available){
+          echo '<div class="row">
+                  <div class="input-field col s12">
+                    Email already used
+                  </div>
+                </div>';
+         } ?>
+        
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="mdi-action-lock-outline prefix"></i>
+            <input id="name" type="text" name ="name">
+            <label for="name">Name</label>
+          </div>
+        </div>
+
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="mdi-action-lock-outline prefix"></i>
+            <input id="password" type="password" name ="password">
+            <label for="password">Password</label>
+          </div>
+        </div>
+
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="mdi-action-lock-outline prefix"></i>
+            <input id="password_again" type="password" name ="password_again">
+            <label for="password_again">Re-type password</label>
+          </div>
+        </div>
+
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="mdi-action-lock-outline prefix"></i>
+            <input id="phone" type="number" name ="phone">
+            <label for="Phone">Phone number</label>
+          </div>
+        </div>
+
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="mdi-action-lock-outline prefix"></i>
+            <input id="credit_number" type="text" name ="credit_number">
+            <label for="credit_number">Credit Card number</label>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="input-field col s12">
+            <?php if(!$pass_match){
+              echo "Password doesnot match";
+            } ?>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="input-field col s12">
+            <button type='submit' name='btn_login' class='btn waves-effect waves-light col s12'>Register</button>
+          </div>
+          <div class="input-field col s12">
+            <p class="margin center medium-small sign-up">Already have an account? <a href="login.php">Login</a></p>
+          </div>
+        </div>
+      </form>
+    </div>
+</body>
+
+
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/js/materialize.min.js"></script>
+
+</html>
