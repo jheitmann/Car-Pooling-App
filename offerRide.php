e376f84f6f35c0300ae42788df10542d83ea7da8
karl soderbarg <kasoderb@kth.se>
2017-10-15 14:22:35 +0800

    offerRide.php

---
+++ offerRide.php
@@ -0,0 +1,25 @@
+<?php
+  session_start();
+  if(!isset($_SESSION['email'])){
+    header("Location: login.php");
+    exit;
+  }
+
+?>
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="w3.css">
+<body>
+  
+<!-- Navigation -->
+<?php require('header.html'); ?>
+
+<!-- Slide Show -->
+
+
+
+
+</body>
+</html>
