80dede6011d2372fd7cbb183e51e466936bdae01
karl soderbarg <kasoderb@kth.se>
2017-10-22 10:45:33 +0800

    small comment

--- insertRide.php
+++ insertRide.php
@@ -1,6 +1,6 @@
 <?php
 
-	require("db_connect.php");
+	require("db_connect.php"); 	#creates the variable $con
 	$insert = "INSERT INTO ride VALUES('".$_POST["carid"]."', '".$_POST["datetime"]."', '".$_POST["origin"]."', '".
                   $_POST["destination"]."', ".$_POST["min"].".00, 'ride300')";
     $insert_return = pg_query($con, $insert);
