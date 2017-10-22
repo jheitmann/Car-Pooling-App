58cf8c6b8447ff09d630a923f7faa4f01ede1edc
jheitmann <julien.heitmann@epfl.ch>
2017-10-22 12:51:51 +0800

    Insert with correct rideid.

--- insertRide.php
+++ insertRide.php
@@ -1,8 +1,12 @@
 <?php
 
 	require("db_connect.php");
+	
+	$max = pg_query("SELECT MAX(rideid) + 1 AS max_id FROM ride");
+	$row = pg_fetch_assoc($max);
+	
 	$insert = "INSERT INTO ride VALUES('".$_POST["carid"]."', '".$_POST["date"]." ".$_POST["hour"].":".$_POST["minute"].":00"."', 
-				'".$_POST["origin"]."', '". $_POST["destination"]."', ".$_POST["min"].".00, 'ride500')";
+				'".$_POST["origin"]."', '". $_POST["destination"]."', ".$_POST["min"].".00, ".$row["max_id"].")";
     $insert_return = pg_query($con, $insert);
 
     if(!$insert_return){
@@ -13,5 +17,3 @@
 	
 	require("offerRide.php");
 ?>
-	
-	
