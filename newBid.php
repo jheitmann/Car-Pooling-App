81f3b02c8889cbd7199d89234af4f75f98c20f5e
karl soderbarg <kasoderb@kth.se>
2017-10-26 15:32:36 +0800

    Completed functional newBid.php

--- newBid.php
+++ newBid.php
@@ -2,29 +2,18 @@
 
 	require("db_connect.php");
 
-	echo " email: ";
-	echo $_POST["email"];
+	$checkBid = "SELECT * FROM bid WHERE client = '".$_POST["email"]."' AND rideid ='".$_POST["rideid"]."'";
 
-	echo " bid: ";
-	echo $_POST["bid"];
-
-	echo " rideid: ";
-	echo $_POST["rideid"];
-
-
-	$checkBid = pg_query($con, "SELECT * FROM bid WHERE client = '".$_POST["email"]."' AND rideid ='".$_POST["rideid"]."'");
-
-	if(!isset($checkBid)){
+	if(pg_fetch_assoc($checkBid)){
 		$insertBid = "INSERT INTO bid VALUES('".$_POST["email"]."', '".$_POST["bid"]."', '".$_POST["rideid"]."') ";
 
 		$insert_return = pg_query($con, $insertBid);
 	    if(!$insert_return){
 			echo "Error: could not insert Bid.";
     	}	
-    }
-
-    else{
-    	$updateBid = "UPDATE bid SET bid_price = '".$_POST["bid"]."' WHERE client = '".$_POST["email"]."' AND rideid = '".$_POST["rideid"]."'";
+    } elseif(!pg_fetch_assoc($checkBid)) {
+    #else {
+    	$updateBid = "UPDATE bid SET bid_price = '".$_POST["bid"]."' WHERE rideid = '".$_POST["rideid"]."'";
 
 	    $update_return = pg_query($con, $updateBid);
 	    if(!$update_return){
@@ -32,14 +21,12 @@
 	    }
     }
 
-
     $update = "UPDATE ride SET price = '".$_POST["bid"]."' WHERE rideid = '".$_POST["rideid"]."'";
 
     $update_return = pg_query($con, $update);
     if(!$update_return){
 		echo "Error: could not update data.";
     }
-
 
 	require("db_close.php");
 	require("ride.php");
