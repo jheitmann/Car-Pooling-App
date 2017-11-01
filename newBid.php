281246f027f6dda97153a1250b793e2df7acff66
karl soderbarg <kasoderb@kth.se>
2017-11-01 18:49:12 +0800

    Fully functional New bid

---
+++ newBid.php
@@ -0,0 +1,35 @@
+<?php
+
+	require("db_connect.php");
+
+	if($_POST["min_bid"] <= $_POST["bid"]){
+		$checkBid = "SELECT * FROM bid WHERE client='".$_POST["email"]."' AND rideid=".$_POST["rideid"];
+		$check = pg_query($con,$checkBid);
+		
+		if(!pg_fetch_assoc($check)){
+			$insertBid = "INSERT INTO bid VALUES('".$_POST["email"]."', ".$_POST["bid"].", ".$_POST["rideid"].") ";
+
+			$insert_return = pg_query($con, $insertBid);
+		    if(!$insert_return){
+				echo "Error: could not insert Bid.";
+	    	}	
+	    } else {
+	    	$updateBid = "UPDATE bid SET bid_price = '".$_POST["bid"]."' WHERE client='".$_POST["email"]."' AND rideid = '".$_POST["rideid"]."'";
+
+		    $update_return = pg_query($con, $updateBid);
+		    if(!$update_return){
+				echo "Error: could not update Bid.";
+		    }
+	    }
+
+	    $update = "UPDATE ride SET price = '".$_POST["bid"]."' WHERE rideid = '".$_POST["rideid"]."'";
+
+	    $update_return = pg_query($con, $update);
+	    if(!$update_return){
+			echo "Error: could not update data.";
+	    }
+
+		require("db_close.php");
+		require("ride.php");
+	}
+?>
