424b61b3d2d6176b26cf0ee018888da0a5aaf63a
Loïc Vandenberghe <loic.vandenberghe@epfl.ch>
2017-11-01 22:16:51 +0800

    create a view ride_price

---
+++ SQL/view.sql
@@ -0,0 +1,5 @@
+CREATE VIEW ride_price AS 
+SELECT distinct r.price , r.carid, r.time_stamp, r.origin, r.destination, r.rideid, b.bid_price
+FROM ride r LEFT OUTER JOIN bid b
+ON (r.rideid = b.rideid 
+AND b.bid_price >= ALL(SELECT b2.bid_price FROM bid b2 WHERE b2.rideid = r.rideid));
