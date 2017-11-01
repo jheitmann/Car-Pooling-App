08d31a043655c8904820798c5b4805283009858c
karl soderbarg <kasoderb@kth.se>
2017-11-01 18:11:58 +0800

    NewBid

--- newBid.php
+++ newBid.php
@@ -2,32 +2,43 @@
 
 	require("db_connect.php");
 
-	$checkBid = "SELECT * FROM bid WHERE client='".$_POST["email"]."' AND rideid=".$_POST["rideid"];
-	$check = pg_query($con,$checkBid);
+	# If statement protecting from change if the current price in the databse is higher than the new bid "bid"
+
 	
-	if(!pg_fetch_assoc($check)){
-		$insertBid = "INSERT INTO bid VALUES('".$_POST["email"]."', ".$_POST["bid"].", ".$_POST["rideid"].") ";
 
-		$insert_return = pg_query($con, $insertBid);
-	    if(!$insert_return){
-			echo "Error: could not insert Bid.";
-    	}	
-    } else {
-    	$updateBid = "UPDATE bid SET bid_price = '".$_POST["bid"]."' WHERE client='".$_POST["email"]."' AND rideid = '".$_POST["rideid"]."'";
+	$bidHackProtection = "SELECT * FROM bid WHERE rideid='".$_POST["rideid"]."' AND price > ".$_POST["bid"];
+	$hackCheck = pg_query($con,$bidHackProtection);
 
-	    $update_return = pg_query($con, $updateBid);
-	    if(!$update_return){
-			echo "Error: could not update Bid.";
+	# Make it so that the first bid can be equal to the "min Prize"
+
+	if(!pg_fetch_assoc($hackCheck)){
+		$checkBid = "SELECT * FROM bid WHERE client='".$_POST["email"]."' AND rideid=".$_POST["rideid"];
+		$check = pg_query($con,$checkBid);
+		
+		if(!pg_fetch_assoc($check)){
+			$insertBid = "INSERT INTO bid VALUES('".$_POST["email"]."', ".$_POST["bid"].", ".$_POST["rideid"].") ";
+
+			$insert_return = pg_query($con, $insertBid);
+		    if(!$insert_return){
+				echo "Error: could not insert Bid.";
+	    	}	
+	    } else {
+	    	$updateBid = "UPDATE bid SET bid_price = '".$_POST["bid"]."' WHERE client='".$_POST["email"]."' AND rideid = '".$_POST["rideid"]."'";
+
+		    $update_return = pg_query($con, $updateBid);
+		    if(!$update_return){
+				echo "Error: could not update Bid.";
+		    }
 	    }
-    }
 
-    $update = "UPDATE ride SET price = '".$_POST["bid"]."' WHERE rideid = '".$_POST["rideid"]."'";
+	    $update = "UPDATE ride SET price = '".$_POST["bid"]."' WHERE rideid = '".$_POST["rideid"]."'";
 
-    $update_return = pg_query($con, $update);
-    if(!$update_return){
-		echo "Error: could not update data.";
-    }
+	    $update_return = pg_query($con, $update);
+	    if(!$update_return){
+			echo "Error: could not update data.";
+	    }
 
-	require("db_close.php");
-	require("ride.php");
+		require("db_close.php");
+		require("ride.php");
+	}
 ?>
